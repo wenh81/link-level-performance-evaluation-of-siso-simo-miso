8258d040e3d5e74a80faf3bbaed71f72ee7f8e2c
SnowzTail <i@snowztail.com>
2019-01-28 13:08:54 +0000

    Update BER plot

--- ber_plot.m
+++ ber_plot.m
@@ -1,5 +1,59 @@
 clear; close all;
 load('ber_set.mat')
+nSnr = length(snrPerBitDb);
+anaBerBpskQpsk = zeros(nSnr, 1);
+%% SIMO MRC
+for iSnr = 1: nSnr
+    % calculate SNR per bit
+    snrPerBit = 10 .^ (snrPerBitDb(iSnr) / 10);
+    % analytical BER
+    anaBerBpskQpsk(iSnr) = 1 / 2 * (1 - sqrt(snrPerBit / (1 + snrPerBit)));
+end
+figure;
+semilogy(snrPerBitDb, anaBerBpskQpsk, 'k:s');
+hold on;
+semilogy(snrDb, numBerQpskMrc, '-.^');
+grid on;
+legend('SISO - ZF', 'SIMO - MRC');
+title('BER vs SNR comparison of SISO ZF and SIMO MRC');
+xlabel('SNR (dB)');
+ylabel('BER');
+close;
+%% MISO MRT
+for iSnr = 1: nSnr
+    % calculate SNR per bit
+    snrPerBit = 10 .^ (snrPerBitDb(iSnr) / 10);
+    % analytical BER
+    anaBerBpskQpsk(iSnr) = 1 / 2 * (1 - sqrt(snrPerBit / (1 + snrPerBit)));
+end
+figure;
+semilogy(snrPerBitDb, anaBerBpskQpsk, 'k:s');
+hold on;
+semilogy(snrDb, numBerQpskMrt, '-.v');
+grid on;
+legend('SISO - ZF', 'MISO - MRT');
+title('BER vs SNR comparison of SISO ZF and MISO MRT');
+xlabel('SNR (dB)');
+ylabel('BER');
+close;
+%% MISO Alamouti
+for iSnr = 1: nSnr
+    % calculate SNR per bit
+    snrPerBit = 10 .^ (snrPerBitDb(iSnr) / 10);
+    % analytical BER
+    anaBerBpskQpsk(iSnr) = 1 / 2 * (1 - sqrt(snrPerBit / (1 + snrPerBit)));
+end
+figure;
+semilogy(snrPerBitDb, anaBerBpskQpsk, 'k:s');
+hold on;
+semilogy(snrDb, numBerQpskAlamouti, '-.v');
+grid on;
+legend('SISO - ZF', 'MISO - Alamouti');
+title('BER vs SNR comparison of SISO ZF and MISO Alamouti');
+xlabel('SNR (dB)');
+ylabel('BER');
+close;
+%% BER comparison
 semilogy(snrDb, numBerQpskAwgn, '-o');
 hold on;
 semilogy(snrDb, numBerQpskZf, '-*');
